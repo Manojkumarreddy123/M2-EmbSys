d23a34715f379ed8f7aafe69967a22fb6032f5f4
Manojkumarreddy123 <98941312+Manojkumarreddy123@users.noreply.github.com>
2022-03-02 10:26:09 +0530

    Update project_main.c

--- Project/3_Implementation/project_main.c
+++ Project/3_Implementation/project_main.c
@@ -117,10 +117,4 @@ String sendData(String command, const int timeout, boolean debug)
         char c = ATmega328.read(); // read the next character.
         response+=c;
       }  
-    }
-    if(debug)
-    {
-      Serial.print(response);
-    }
-    return response;
-}
+    
